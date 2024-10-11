0ecdf03096594d7257fe531cffd3be131daaa338
saulomiranda0021 <saulomiranda1415@gmail.com>
2024-10-11 16:37:15 -0300

    Arquivo do projeto.

---
+++ Checklist/tarefa/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import Tarefa
+
+class TarefaForm(forms.ModelForm):
+    class Meta:
+        model = Tarefa
+        fields = '__all__'
+        widgets = {
+            'nome_tarefa': forms.TextInput(attrs={'class': 'input', 'placeholder': 'Digite a tarefa'})
+        }
